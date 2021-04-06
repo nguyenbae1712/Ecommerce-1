6c2a0ce7e717f9df66fa237a4aaabc2edc766283
nguyenbae1712 <34834564+nguyenbae1712@users.noreply.github.com>
2021-04-06 12:02:17 +0700

    database

--- EcommerceDB/shop_online_order.sql
+++ EcommerceDB/shop_online_order.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `order`
 --
-
+use ecommerce_db;
 DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
